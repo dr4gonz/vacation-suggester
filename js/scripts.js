d5616b99040b407fbc18446c693bd0152a7a020d
Matthew Reyes <mreyez@gmail.com>
2016-06-03 09:50:04 -0700

    removed repetative logic and replaced with for loop to iterate over answers array

--- js/scripts.js
+++ js/scripts.js
@@ -11,109 +11,24 @@ $(document).ready(function(){
     var q9 = $("input:radio[name=question9]:checked").val();
     var q10 = $("input:radio[name=question10]:checked").val();
 
+    var answers = [q1,q2,q3,q4,q5,q6,q7,q8,q9,q10];
+
     var tulum = 0;
     var paris = 0;
     var serengeti = 0;
     var alaska = 0;
 
-    if (q1 === "tulum-ans") {
-      tulum++;
-    } else if (q1 === "paris-ans") {
-      paris++;
-    } else if (q1 === "serengeti-ans") {
-      serengeti++;
-    } else if (q1 === "alaska-ans") {
-      alaska++;
-    }
-
-    if (q2 === "tulum-ans") {
-      tulum++;
-    } else if (q2 === "paris-ans") {
-      paris++;
-    } else if (q2 === "serengeti-ans") {
-      serengeti++;
-    } else if (q2 === "alaska-ans") {
-      alaska++;
-    }
-
-    if (q3 === "tulum-ans") {
-      tulum++;
-    } else if (q3 === "paris-ans") {
-      paris++;
-    } else if (q3 === "serengeti-ans") {
-      serengeti++;
-    } else if (q3 === "alaska-ans") {
-      alaska++;
-    }
-
-    if (q4 === "tulum-ans") {
-      tulum++;
-    } else if (q4 === "paris-ans") {
-      paris++;
-    } else if (q4 === "serengeti-ans") {
-      serengeti++;
-    } else if (q4 === "alaska-ans") {
-      alaska++;
-    }
-
-    if (q5 === "tulum-ans") {
-      tulum++;
-    } else if (q5 === "paris-ans") {
-      paris++;
-    } else if (q5 === "serengeti-ans") {
-      serengeti++;
-    } else if (q5 === "alaska-ans") {
-      alaska++;
-    }
-
-    if (q6 === "tulum-ans") {
-      tulum++;
-    } else if (q6 === "paris-ans") {
-      paris++;
-    } else if (q6 === "serengeti-ans") {
-      serengeti++;
-    } else if (q6 === "alaska-ans") {
-      alaska++;
-    }
-
-    if (q7 === "tulum-ans") {
-      tulum++;
-    } else if (q7 === "paris-ans") {
-      paris++;
-    } else if (q7 === "serengeti-ans") {
-      serengeti++;
-    } else if (q7 === "alaska-ans") {
-      alaska++;
-    }
-
-    if (q8 === "tulum-ans") {
-      tulum++;
-    } else if (q8 === "paris-ans") {
-      paris++;
-    } else if (q8 === "serengeti-ans") {
-      serengeti++;
-    } else if (q8 === "alaska-ans") {
-      alaska++;
-    }
-
-    if (q9 === "tulum-ans") {
-      tulum++;
-    } else if (q9 === "paris-ans") {
-      paris++;
-    } else if (q9 === "serengeti-ans") {
-      serengeti++;
-    } else if (q9 === "alaska-ans") {
-      alaska++;
-    }
-
-    if (q10 === "tulum-ans") {
-      tulum++;
-    } else if (q10 === "paris-ans") {
-      paris++;
-    } else if (q10 === "serengeti-ans") {
-      serengeti++;
-    } else if (q10 === "alaska-ans") {
-      alaska++;
+    //iterates of answers[] to tally answers
+    for (i=0; i < answers.length-1; i++){
+      if (answers[i] === "tulum-ans") {
+        tulum++;
+      } else if (answers[i] === "paris-ans") {
+        paris++;
+      } else if (answers[i] === "serengeti-ans") {
+        serengeti++;
+      } else if (answers[i] === "alaska-ans") {
+        alaska++;
+      }
     }
 
     console.log(tulum);
